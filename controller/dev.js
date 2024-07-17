a59da8d50482a2b93ec0b8bc6e0cd1e7e7d9d588
playcsgo <sakura_0125@hotmail.com>
2024-07-17 13:39:53 +0800

    DI at controller

--- controller/dev.js
+++ controller/dev.js
@@ -1,11 +1,21 @@
 /* eslint-disable class-methods-use-this */
 const ApiError = require('../error/api-error');
-const devService = require('../service/dev'); // 這樣會有個問題, 因為要測試這個controller, 你必須要確定devService完全沒有問題
+// const devService = require('../service/dev'); // 這樣會有個問題, 因為要測試這個controller, 你必須要確定devService完全沒有問題
+
+
 
 class DevController {
+  constructor({ devService }) {
+    this.devService = devService
+    // bind(this) 之後, this.createDev 跟 this.getDev 的this永遠指向 DevController
+    // 這樣才能訪問到 this.devService
+    this.createDev = this.createDev.bind(this)
+    this.getDev = this.getDev.bind(this)
+  }
+
   async createDev(req, res) {
     try {
-      const result = await devService.createDev(req.body);
+      const result = await this.devService.createDev(req.body);
       res.status(201).json(result);
     } catch (err) {
       console.error(err);
@@ -16,7 +26,7 @@ class DevController {
   async getDev(req, res, next) {
     try {
       const developerId = req.params.id;
-      const developer = await devService.getDev(req.params.id);
+      const developer = await this.devService.getDev(req.params.id);
       if (developer == null) {
         next(ApiError.notFound(`developer with id ${developerId} not found`));
         return;
